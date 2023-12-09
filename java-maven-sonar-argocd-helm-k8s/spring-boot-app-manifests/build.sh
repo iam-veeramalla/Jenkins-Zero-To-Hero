9d87b32c8c082bbbcf8f18784759dc0f949111ff
ramanjk <30591604+ramanjk@users.noreply.github.com>
2023-12-09 19:15:22 +0530

    Update build.sh

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/build.sh
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BUILD_NUMBER=${BUILD_NUMBER}
-sed -i "s/mydevopsacr1.azurecr.io\/ultimate-cicd:.*/mydevopsacr1.azurecr.io\/ultimate-cicd:${BUILD_NUMBER}/g" java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/deployment.yml
+sed -i "s/mydevopsacr1.azurecr.io\/ultimate-cicd:.*/mydevopsacr1.azurecr.io\/ultimate-cicd:${BUILD_NUMBER}/g" CD/deployment.yml
