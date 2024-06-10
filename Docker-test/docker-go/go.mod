cc77304aa0e47eeea7029dca8c0fe0c6dd66bd1c
Venkatsaidevisetty <152339122+Venkatsaidevisetty@users.noreply.github.com>
2024-06-10 16:22:11 -0500

    Add files via upload

---
+++ Docker-test/docker-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/yourusername/go-hello-world
+
+go 1.16
---
+++ Docker-test/docker-nodejs/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
